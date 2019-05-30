1dcbc2ebe2f7a1f62d5c7305458e697c73ec6389
Wouter Van Dam <wouter.vandam@kbc.be>
2019-05-30 22:33:38 +0200

    temp work for mealdb

---
+++ src/main/java/com/kbc/starwars/contract/IngredientResource.java
@@ -0,0 +1,14 @@
+package com.kbc.starwars.contract;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class IngredientResource {
+
+    private String quantity;
+    private String name;
+}
